deb32a4cfe1e86f7aa16c7e0ddb2236722f92671
Matthew Butler <matthewtbutler@gmail.com>
2023-11-25 12:12:38 -0600

    option to extract png or pdf

--- extract-image.py
+++ extract-image.py
@@ -15,41 +15,46 @@ def extract_base64_image(html_content):
         return match.group(1)
     else:
         return None
+    
+def save_image_as_png(image_data, png_filename):
+    image = Image.open(image_data)
+    image.save(png_filename, 'PNG')
 
 def save_image_to_pdf(image_data, pdf_filename):
     # Create a PDF
     c = canvas.Canvas(pdf_filename, pagesize=letter)
 
     # Define image size and position
-    image_width = 500  # Adjust as needed
-    image_height = 500  # Adjust as needed
+    image_width = 400  # Adjust as needed
+    image_height = 400  # Adjust as needed
     x_position = (letter[0] - image_width) / 2  # Center horizontally
     y_position = (letter[1] - image_height) / 2 + 75  # Center vertically
 
     # Draw the image onto the PDF
     c.drawImage(ImageReader(image_data), x_position, y_position, width=image_width, height=image_height)
 
-    # Add text (filename)
-    text_x_position = 110  # Adjust as needed
-    text_y_position = 120   # Adjust as needed
-    hash_value = "13c78c707b010724cd9e1f596b58246a2c829384fc8a8a4b49aa38b3fddfc1c2"
-    c.drawString(text_x_position, text_y_position, os.path.basename(hash_value))
-
     # Save the PDF
     c.save()
+
+def save_image(image_data, format, original_filename):
+    if format == 'pdf':
+        pdf_filename = original_filename.replace('.html', '.pdf')
+        save_image_to_pdf(image_data, pdf_filename)
+    elif format == 'png':
+        png_filename = original_filename.replace('.html', '.png')
+        save_image_as_png(image_data, png_filename)
+    else:
+        print(f"Unsupported format: {format}")
 
 # Iterate over all HTML files in the current directory
 for filename in os.listdir('.'):
     if filename.endswith('.html'):
-        # Open and read the HTML file
         with open(filename, 'r') as file:
             html_content = file.read()
 
-        # Extract and decode the base64 image
         base64_image = extract_base64_image(html_content)
         if base64_image:
             image_data = io.BytesIO(base64.b64decode(base64_image))
-            pdf_filename = filename.replace('.html', '.pdf')
-            save_image_to_pdf(image_data, pdf_filename)
+            save_image(image_data, 'pdf', filename)  # Or 'png' based on requirement
         else:
             print(f"No base64 encoded image found in {filename}.")
